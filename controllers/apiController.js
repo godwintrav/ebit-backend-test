81bebc6df41c1aa368e9cca3d9ec9a3dd248b6fe
Odenigbo Godwin <godwintrav@gmail.com>
2022-08-24 12:54:21 +0000

    Step 2: Return the ETH/GBP price

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -1,8 +1,8 @@
 var database = require("../database/database.js");
 
 module.exports.fetchRecentETHUSDPrice = async (req, res) => {
-    var sqlQuery = "SELECT vwap FROM market_price ORDER BY startTime DESC LIMIT 0, 1";
-    var parameters = [];
+    var sqlQuery = "SELECT vwap FROM 'market_price' WHERE pair = ? ORDER BY startTime DESC LIMIT 0, 1";
+    var parameters = ["ETH/USD"];
     database.all(sqlQuery, parameters, (err, row) => {
         if(err) {
             res.status(400).json({"error": err.message});
@@ -13,3 +13,18 @@ module.exports.fetchRecentETHUSDPrice = async (req, res) => {
     });
 
 }
+
+module.exports.fetchRecentETHGBPPrice = async (req, res) => {
+    var sqlQuery = "SELECT vwap FROM 'market_price' WHERE pair = ? ORDER BY startTime DESC LIMIT 0, 1";
+    var parameters = ["ETH/GBP"];
+    database.all(sqlQuery, parameters, (err, row) => {
+        if(err) {
+            res.status(400).json({"error": err.message});
+            return;
+        }
+        let data = row[0];
+        res.json({"pair": "ETH/GBP", "vwap": data["vwap"]});
+    });
+
+}
+
--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -3,5 +3,7 @@ const router = Router();
 const apiController = require("../controllers/apiController.js");
 
 router.get("/fx/ohlc/ETHUSD", apiController.fetchRecentETHUSDPrice);
+router.get("/fx/ohlc/ETHGBP", apiController.fetchRecentETHGBPPrice);
+router.get("/fx/ohlc/ETHUSD/history", apiController.fetchHighAndLowPricesETHUSD);
 
 module.exports = router;
